b080410ceb6e783c0aa4e6749e9e8db9dc7349d4
jemmacrosby <jemmajsc@outlook.com>
2024-08-26 10:48:45 +0100

    Fix nav bar dropdown

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import caretUpSVG from '../assets/caret-up.svg'
+import caretDownSVG from '../assets/caret-down.svg'
 
 interface DropdownProps {
   title: string;
@@ -7,19 +9,27 @@ interface DropdownProps {
 
 const Dropdown: React.FC<DropdownProps> = ({ title, children }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [caretUp, setCaretUp] = useState(true);
 
-  const toggleDropdown = () => setIsOpen(!isOpen);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen)
+    setCaretUp(!caretUp)
+  };
 
   return (
     <div className="relative">
       <button
         onClick={toggleDropdown}
-        className='text-xl p-2 rounded'
+        className='text-xl p-2'
       >
-        {title}
+        <div className='flex items-center gap-1'>
+          {title}
+          {caretUp ? <img src={caretUpSVG} className='h-5'/> : <img src={caretDownSVG} className='h-5'/>}
+        </div>
+
       </button>
       {isOpen && (
-        <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg">
+        <div className="absolute mt-2 w-48 bg-white border border-gray-200 shadow-lg">
           {children}
         </div>
       )}
