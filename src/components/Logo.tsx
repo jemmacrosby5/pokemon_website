bda91763ad53902ebde1a75041d6f1c501d12763
jemmacrosby <jemmajsc@outlook.com>
2024-08-26 16:06:48 +0100

    Basic working mobile navbar

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,8 +5,8 @@ function Logo() {
     return (
       <>
       <a href="/">
-        <div className='flex flex-row gap-2 items-center'>
-            <img className='h-12 min-w-24' src={logo} alt="" />
+        <div className='flex flex-row gap-2 items-center mx-auto'>
+            <img className='h-12 min-w-20' src={logo} alt="" />
             <p className="text-xl"> PokéJave </p>
         </div>
         </a>
--- src/components/NavBarMobile.tsx
+++ src/components/NavBarMobile.tsx
@@ -1,22 +1,37 @@
-import Logo from "./Logo"
-import MenuIcon from '../assets/menu.svg'
-import { NavBarBackground } from '../styles/NavBarStyles.ts';
-import { useState } from "react";
+import { useState } from "react"
+import caretUpSVG from '../assets/caret-up-black.svg'
+import caretDownSVG from '../assets/caret-down-black.svg'
 
-const NavBar: React.FC = () => {
-    const [showMenu, setShowMenu] = useState(false);
+const NavBarMobile: React.FC = () => {
+  const [showSets, setShowSets] = useState(false);
 
-    const openMenu = () => {
-        setShowMenu(!openMenu)
-    }
+  const toggleSets = () => {
+    setShowSets(!showSets);
+  }
+
   return (
     <>
-        <NavBarBackground>
-            <img className="ml-1" src={MenuIcon}/>
-            <Logo/>
-        </NavBarBackground>
+      <div className="flex flex-col gap-4 pt-2 p-4 bg-white rounded-sm text-black">
+        <a href="/cards" className="text-xl p-2">Cards</a>
+        <button onClick={toggleSets} className="none">
+          <div className="flex items-center">
+            <p className="text-xl p-2">Sets</p>
+            {showSets ? <img src={caretUpSVG} className='h-5' /> : <img src={caretDownSVG} className='h-5' />}
+          </div>
+        </button>
+        {showSets && (<div>
+          <a href="/sets" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Sets Homepage</a>
+          <a href="/sets/rubysapphire" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Ruby Sapphire</a>
+          <a href="/sets/celebrations" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Celebrations</a>
+          <a href="/sets/151" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">151</a>
+        </div>)}
+
+        <a href="/about" className="text-xl p-2">About</a>
+      </div>
     </>
   )
+
+
 }
 
-export default NavBar
+export default NavBarMobile
